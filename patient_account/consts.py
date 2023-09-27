89da04ed02e6e88ac2ebe72beeca278ef3fd1fc0
Olga Lipina <neon_alfa@mail.ru>
2023-09-27 12:47:16 +0000

    Removed prints.

--- patient_account/consts.py
+++ patient_account/consts.py
@@ -8,9 +8,6 @@ PROJECT_PATH = Path(__file__).parent.parent
 ENV_PATH = PROJECT_PATH / '.env'
 config_env = dotenv_values(ENV_PATH)
 
-print('env file:\n', config_env)
-print('SERVICE_HOST from ENV VAR:\n', os.environ.get("SERVICE_POR"))
-
 SERVICE_HOST = os.environ.get('SERVICE_HOST', config_env.get("SERVICE_HOST"))
 SERVICE_PORT = os.environ.get('SERVICE_PORT', config_env.get("SERVICE_PORT"))
 
